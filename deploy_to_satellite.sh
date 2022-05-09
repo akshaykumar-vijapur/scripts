e03c0b80c10867354295f68b40693069342fb68f
akshay vijapur <akshaykumar.vijapur@ibm.com>
2022-05-08 21:58:40 -0700

    update sat feedback

--- deploy_to_satellite.sh
+++ deploy_to_satellite.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 function getAPPUrlSatConfig() {
+  for filename in $(find /artifacts/${MANIFEST_DIR} -type f -print); do   
+    echo "file name is ${filename}"
+    route_name=$(yq -o=json eval ${filename} |   jq -r  'select(.kind=="Route") | .metadata.name')  
+     if [ ! -z "${route_name}" ]; then
+      break;
+    fi
+  done
 
-  route_name=$(yq -o=json eval ${DEPLOYMENT_FILE} |   jq -r  'select(.kind=="Route") | .metadata.name')
   if [ -z "${route_name}" ]; then
     echo "Did not find deployment of type route. Unable to fetch the application url....."
     return
@@ -10,7 +16,7 @@ function getAPPUrlSatConfig() {
 
   for ITER in {1..30}
     do
-        resources_ids=$(ic sat resource ls -output json |  jq -r ".resources.resources[] | select(.searchableData.name==\"$route_name\") | .id ")
+        resources_ids=$(ic sat resource ls -output json |  jq -r ".resources.resources[] | select(.searchableData.name==\"$route_name\") | select(.searchableData.namespace==\"$CLUSTER_NAMESPACE\") | .id ")
         resource_id=$(echo "${resources_ids}" | awk '{print $1}')
         if [ -z "${resource_id}" ]
         then
@@ -30,45 +36,44 @@ function getAPPUrlSatConfig() {
 function createAndDeploySatelliteConfig() {
 
 echo "=========================================================="
-SAT_CONFIG=$1
-SAT_CONFIG_VERSION=$2
-KUBE_RESOURCE=$3
+APP_NAME=$1
+SAT_CONFIG=$2
+SAT_CONFIG_VERSION=$3
 DEPLOY_FILE=$4
-echo "Creating config for $SAT_CONFIG...."
+echo "Creating config for ${SAT_CONFIG}...."
 
-export SATELLITE_SUBSCRIPTION="$SAT_CONFIG-$SATELLITE_CLUSTER_GROUP"
+export SATELLITE_SUBSCRIPTION="${APP_NAME}-${SAT_CONFIG}"
 export SAT_CONFIG_VERSION
-if ! ic sat config version get --config "$SAT_CONFIG" --version "$SAT_CONFIG_VERSION" &>/dev/null; then
-  echo -e "Current resource ${KUBE_RESOURCE} not found in ${IBMCLOUD_IKS_CLUSTER_NAMESPACE}, creating it"
-  if ! ibmcloud sat config get --config "$SAT_CONFIG" &>/dev/null ; then
-    ibmcloud sat config create --name "$SAT_CONFIG"
+if ! ic sat config version get --config "${APP_NAME}" --version "${SAT_CONFIG_VERSION}" &>/dev/null; then
+  echo -e "Current resource ${SAT_CONFIG_VERSION} not found, creating it"
+  if ! ibmcloud sat config get --config "${APP_NAME}" &>/dev/null ; then
+    ibmcloud sat config create --name "${APP_NAME}"
   fi
   echo "deployment file is ${DEPLOY_FILE}"
-  ibmcloud sat config version create --name "$SAT_CONFIG_VERSION" --config "$SAT_CONFIG" --file-format yaml --read-config "${DEPLOY_FILE}"
+  ibmcloud sat config version create --name "${SAT_CONFIG_VERSION}" --config "${APP_NAME}" --file-format yaml --read-config "${DEPLOY_FILE}"
 else
-  echo -e "Current resource ${KUBE_RESOURCE} already found in ${IBMCLOUD_IKS_CLUSTER_NAMESPACE}"
+  echo -e "Current resource ${SAT_CONFIG_VERSION} already found."
 fi
 
-EXISTING_SUB=$(ibmcloud sat subscription ls -q | grep "$SATELLITE_SUBSCRIPTION" || true)
+EXISTING_SUB=$(ibmcloud sat subscription ls -q | grep "${SATELLITE_SUBSCRIPTION}" || true)
   if [ -z "${EXISTING_SUB}" ]; then
-    ibmcloud sat subscription create --name "$SATELLITE_SUBSCRIPTION" --group "$SATELLITE_CLUSTER_GROUP" --version "$SAT_CONFIG_VERSION" --config "$SAT_CONFIG"
+    ibmcloud sat subscription create --name "${SATELLITE_SUBSCRIPTION}" --group "${SATELLITE_CLUSTER_GROUP}" --version "${SAT_CONFIG_VERSION}" --config "${APP_NAME}"
   else
-    ibmcloud sat subscription update --subscription "$SATELLITE_SUBSCRIPTION" -f --group "$SATELLITE_CLUSTER_GROUP" --version "$SAT_CONFIG_VERSION"
+    ibmcloud sat subscription update --subscription "${SATELLITE_SUBSCRIPTION}" -f --group "${SATELLITE_CLUSTER_GROUP}" --version "${SAT_CONFIG_VERSION}"
 fi
 }
 
-
+ls -laht /artifacts/${MANIFEST_DIR}
+commit=$(git log -1 --pretty=format:%h)
 for filename in $(find /artifacts/${MANIFEST_DIR} -type f -print); do   
-  echo "file name is ${filename}"  
-  echo " " >> ${DEPLOYMENT_FILE}
-  echo "---" >> ${DEPLOYMENT_FILE}
-  yq e ".metadata.namespace = \"${CLUSTER_NAMESPACE}\"" ${filename} >> ${DEPLOYMENT_FILE}
+  echo "file name is ${filename}" 
+  config_name=$(basename ${filename} | cut -d. -f1)
+  config_name_version=${config_name}_${commit} 
+  #echo "updating the namespaces in the deployment file."
+  #yq e ".metadata.namespace = \"${CLUSTER_NAMESPACE}\"" ${filename} >> test_${filename}
+  createAndDeploySatelliteConfig ${APP_NAME} ${config_name} ${config_name_version} ${filename}
 done
 
-
-yq e  -i '.metadata.labels.razee/watch-resource = "lite"' ${DEPLOYMENT_FILE}
-commit=$(git log --format="%H" -n 1)
-createAndDeploySatelliteConfig ${APP_NAME} ${commit} deployment_file ${DEPLOYMENT_FILE} 
 getAPPUrlSatConfig
 echo "APPURL ${APPURL}"
 
